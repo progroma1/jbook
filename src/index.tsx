0e5fd583cd556ee48a6cf988f29e8268efd3947d
Roman Tyniv <roman.tyniv@3ss.tv>
2021-12-30 13:58:18 +0200

    fixed for css and jsx filed etc

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM  from "react-dom";
 import { useState, useEffect, useRef } from 'react';
 import * as esbuild from 'esbuild-wasm';
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
 
@@ -13,7 +14,7 @@ const App = () => {
     const startService = async () => {
         ref.current = await esbuild.startService({
             worker: true,
-            wasmURL: '/esbuild.wasm'
+            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
         });
     }
 
@@ -30,7 +31,11 @@ const App = () => {
             entryPoints: ['index.js'],
             bundle: true,
             write: false,
-            plugins: [unpkgPathPlugin()],
+            plugins: [
+                unpkgPathPlugin(),
+                fetchPlugin(input),
+            ],
+
             define: {
                 'process.env.NODE_ENV': '"production"',
                 global: 'window',
